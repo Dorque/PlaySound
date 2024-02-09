1c671ab515ad9c6fcb42ad77fada08a5811f5e87
Anthony Dang <danghtanthony@gmail.com>
2024-02-08 19:48:38 -0700

    Initial commit

---
+++ PlaySound/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Media;
+using NAudio;
+using NAudio.CoreAudioApi;
+using NAudio.Wave;
+
+public class PlayToDevice
+{
+    public static void Main(string[] args)
+    {
+        if (args.Length < 1)
+        {
+            Console.WriteLine("Usage: PlayToDevice <sound_file_path>");
+            return;
+        }
+
+        string filePath = args[0];
+
+        var enumerator = new MMDeviceEnumerator();
+        var device = enumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Communications);
+
+        using(var audioFile = new AudioFileReader(filePath))
+        using(var outputDevice = new WasapiOut(device, AudioClientShareMode.Shared, true, 0))
+        {
+            outputDevice.Init(audioFile);
+            outputDevice.Play();
+            while (outputDevice.PlaybackState == PlaybackState.Playing)
+            {
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# PlaySound
+Plays sound on the default communication device using command line
+
+Usage: PlaySound.exe <sound_file_path>
